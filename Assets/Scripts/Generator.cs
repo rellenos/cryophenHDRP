d0c8a45547d15a455853006980573484c4bbbe27
rellenos <danieling00@gmail.com>
2022-04-01 00:51:13 +0200

    texturas

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+
+    public Material[] materialsToChooseFrom;
+    public Renderer thisRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisRenderer.material = materialsToChooseFrom[Random.Range(0, materialsToChooseFrom.Length)];
+    }
+    
+}
+
